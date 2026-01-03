127809ed393325d502c483d12bcaf26de95aaf76
MatiushkoDasha <zmiyka47gmail.com>
2026-01-03 23:40:58 +0200

    js

---
+++ java/script.js
@@ -0,0 +1,7 @@
+const black = document.querySelector(".black"),
+      bc = document.querySelector(".bc")
+
+black.addEventListener("click", function() {
+black.style.opacity = "0"
+bc.style.opacity = "0"
+})
