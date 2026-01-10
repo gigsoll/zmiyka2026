f3aa90c767c8cf2c31b3f52d413cd3b8c3d6d469
MatiushkoDasha <zmiyka47gmail.com>
2026-01-10 10:11:27 +0200

    new shlah

--- java/card-generator.js
+++ java/card-generator.js
@@ -124,7 +124,7 @@ async function readData(dataPath) {
 document.addEventListener('DOMContentLoaded', async function() {
     // Name of class of the card wrapper container
     const parentName = "tovar_main";
-    const dataLocation = "../data.json"
+    const dataLocation = "https://raw.githubusercontent.com/MatiushkoDasha/zmiyka2026/refs/heads/master/data.json"
 
     // Load data from JSON file
     data = await readData(dataLocation)
